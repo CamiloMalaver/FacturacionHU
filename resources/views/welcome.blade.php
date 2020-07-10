39f89f6edcb2fd31ad98f58e19a3bc00b3a972f6
CamiloMalaver <62223822+CamiloMalaver@users.noreply.github.com>
2020-07-09 19:33:36 -0500

    JS y Jquery Bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        
+
         <title>Facturación</title>
 
         <!-- Fonts -->
@@ -82,13 +81,13 @@
                 <div class="row">
                     <br>
                     <div class="col links">             
-                        <button type="button" class="btn btn-info btn-lg btn-block font-weight-bold">Crear</button>
+                        <button type="button" class="btn btn-info btn-lg font-weight-bold" data-toggle="modal" data-target="#exampleModal">Crear</button>
                     </div>                    
                     <div class="col links">             
-                        <button type="button" class="btn btn-info btn-lg btn-block font-weight-bold">Archivos</button>
+                        <button type="button" class="btn btn-info btn-lg font-weight-bold">Archivos</button>
                     </div>   
                     <div class="col links">             
-                        <button type="button" class="btn btn-info btn-lg btn-block font-weight-bold">Clientes</button>
+                        <button type="button" class="btn btn-info btn-lg font-weight-bold">Clientes</button>
                     </div>                      
                     <br>              
                 </div>
@@ -96,3 +95,31 @@
         </div>
     </body>
 </html>
+
+<!--Scripts-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+<!--Ventanas modales-->
+
+    <!-- Modal Crear-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+    </div>
+    </div>
