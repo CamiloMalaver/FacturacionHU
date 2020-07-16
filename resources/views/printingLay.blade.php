9c2d72017ec161d21dc3165e17b5517027eaae17
CamiloMalaver <62223822+CamiloMalaver@users.noreply.github.com>
2020-07-16 18:02:39 -0500

    Avance en plantilla Printing

---
+++ resources/views/printingLay.blade.php
@@ -0,0 +1,63 @@
+<!--Styles-->
+    <style>
+        .texthu{
+            font-family: Verdana;
+            font-size: 16;
+            color: rgb(115, 188, 167);
+        }
+    </style>
+<div class="container">
+    <!--Header-->
+    <div class="row align-items-center">
+        <div class="col-9">
+            <img src="{{asset('../resources/img/info.png')}}" width="400">
+        </div>
+        <div class="col-3">
+            <p style="font-family: Verdana; font-size:25; color: rgb(115, 188, 167)">Cotización N° 15</p>
+        </div>
+    </div>  
+    <br>
+    <!--Client info-->
+    <div style="border:solid; border-width:2px; border-radius:5px; border-color:rgb(115, 188, 167);">
+        <div class="row">
+            <div class="col"><span class="texthu">FECHA DE EXPEDICIÓN</span>bgfndfndrn</div>
+            <div class="col"><span class="texthu">FECHA DE VENCIMIENTO</span>htr4ehtreth</div>
+        </div>
+        <div class="row">
+            <div class="col"><span class="texthu">SEÑORES</span>bgfndfndrn</div>
+            <div class="col"><span class="texthu">CC O NIT</span>htr4ehtreth</div>
+        </div>
+        <div class="row">
+            <div class="col"><span class="texthu">DIRECCIÓN</span>bgfndfndrn</div>
+            <div class="col"><span class="texthu">TELÉFONO</span>htr4ehtreth</div>
+        </div>         
+    </div>
+    <br>
+    <!--Body-->  
+    <table class="table table-striped">
+        <thead>
+            <tr>
+            <th scope="col">CANT</th>
+            <th scope="col">DESCRIPCIÓN</th>
+            <th scope="col">Vr. UNITARIO</th>
+            <th scope="col">Vr. TOTAL</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>            
+        </tbody>
+    </table>
+
+</div>
+
+<!--Scripts-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function(){return view('printingLay');});
 Route::get('/', 'index@getView');
 Route::get('Archivos', 'archivos@getView')->name('getArchivosView');
 Route::get('Clientes', 'clientes@getView')->name('getClientesView');
